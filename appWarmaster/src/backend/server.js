e5f9cf40d9214b0e42aec00d34c4b7341179aa3a
Davicin36 <dalvroc96@gmail.com>
2025-12-03 13:34:03 +0100

    update server y cambios en css para seleccion juegos

--- appWarmaster/src/backend/server.js
+++ appWarmaster/src/backend/server.js
@@ -48,7 +48,7 @@ const opcionesCors = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Content-Range', 'X-Content-Range'],
   optionsSuccessStatus: 200
