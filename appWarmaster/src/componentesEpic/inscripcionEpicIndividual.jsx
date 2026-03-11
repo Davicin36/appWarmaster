09e1830b37e3869e39dc73fcb16d5dd3ba250a9a
Davicin36 <dalvroc96@gmail.com>
2026-03-11 18:28:46 +0100

    arreglo

--- appWarmaster/src/componentesEpic/inscripcionEpicIndividual.jsx
+++ appWarmaster/src/componentesEpic/inscripcionEpicIndividual.jsx
@@ -1,4 +1,4 @@
-// componentesSaga/inscripciones/inscripcionSagaIndividual.jsx
+// componentesEpic/inscripcionEpicIndividual.jsx
 
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
